70e68ab981e252de21b45c0a81f63720e9c0452f
points14514 <3790010758@qq.com>
2025-10-04 11:50:17 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,27 @@
+echo "First, you have to install some libraries"
+ 
+pip install pyyaml
+ 
+pip install requests
+
+pip install distro
+ 
+pip install psutil
+ 
+pip install ujson
+ 
+pip install docker
+# If you are rhel, then you can carry out this life
+# sudo dnf install python3-dnf
+# Or are you termux, If a command is missing, you can install it(For example:ldd)
+# pkg install ldd
+echo "Then, install the required compilers"
+pip install pyinstaller
+pyinstaller -F main.py --name mtosme-pkg
+echo "Compilation complete, you can use it normally"
+mv mtosme-pkg ~/../usr/bin
+# Or you're not termux. You can also carry out this order
+# mv mtosme-pkg /usr/bin
+# Remember. Don't put it down /usr/lib, because this is not the directory where the binaries are
+sleep 9
+exit 1
